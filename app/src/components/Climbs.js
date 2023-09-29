1d259e46cfc24b969a46d8be0892c24c7d907c97
joshuazhou744 <joshuazhou744@gmail.com>
2023-09-29 12:34:11 -0600

    fix bg

--- app/src/components/Climbs.js
+++ app/src/components/Climbs.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
 const Climbs = () => {
+
+  const climbs = [
+    {
+      grade: 'red',
+      date: '09/29',
+      link: 'https://www.youtube.com/watch?v=QjRFZWLl5-A'
+    },
+    {
+      grade: 'green',
+      date: '09/28',
+      link: 'hi'
+    }
+  ]
+
   return (
     <div className='main'>
       Climbs
+      <h1>{climbs[0].grade}</h1>
     </div>
   )
 }
--- app/src/components/grades/Yellow.js
+++ app/src/components/grades/Yellow.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Yellow() {
   return (
     <div>
-      
+      Yellow
     </div>
   )
 }
